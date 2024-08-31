781ff74c79e42accfd95f999ac081d5d2db902be
seriouskang <scdean110@gmail.com>
2024-08-31 23:53:19 +0900

    feat(tf): add output vars.tf

---
+++ terraform/output/vars.tf
@@ -0,0 +1,14 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "ap-northeast-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-northeast-1 = "ami-0a0b7b240264a48d7"
+    foo            = "bar"
+  }
+}
