971acf3f0b2fc2df9553165f969a6ab4ebaa7502
seriouskang <scdean110@gmail.com>
2024-08-31 19:46:15 +0900

    feat(tf): add sw-provision vars.tf

--- terraform/sw-provision/vars.tf
+++ terraform/sw-provision/vars.tf
@@ -1,8 +1,16 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
   default = "ap-northeast-1"
 }
+
+variable "PATH_TO_PRIVATE_KEY" {}
+variable "PATH_TO_PUBLIC_KEY" {}
+variable "INSTANCE_USERNAME" {
+    default = "ubuntu"
+}
+
 variable "AMIS" {
   type = map(string)
   default = {
