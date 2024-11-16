5b53dd9dd234f06a64966bd0062ab2fdc3b57cf3
seriouskang <scdean110@gmail.com>
2024-11-16 19:43:30 +0900

    feat: NAT 신규

--- terraform/vpc/nat.tf
+++ terraform/vpc/nat.tf
@@ -1,3 +1,9 @@
 resource "aws_eip" "nat" {
   domain = "vpc"
 }
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.main-public-1.id
+  depends_on = [ aws_internet_gateway.main-igw ]
+}
