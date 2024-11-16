610cb6f3e17019d4c196dbefeffa797688210fd4
seriouskang <scdean110@gmail.com>
2024-11-16 20:33:13 +0900

    feat: key pair 신규

--- terraform/aws-basic/vars.tf
+++ terraform/aws-basic/vars.tf
@@ -3,3 +3,11 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "ap-northeast-1"
 }
+
+variable "PATH_TO_PRIAVTE_KEY" {
+  default = "test-key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "test-key.pub"
+}
