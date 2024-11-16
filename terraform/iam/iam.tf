269e02cedaaf3e82dfbfa397457ca846f4bdea03
seriouskang <scdean110@gmail.com>
2024-11-16 23:18:00 +0900

    feat: iam policy attachment 추가

--- terraform/iam/iam.tf
+++ terraform/iam/iam.tf
@@ -1,3 +1,9 @@
 resource "aws_iam_group" "admin" {
   name = "admin"
 }
+
+resource "aws_iam_policy_attachment" "admin-attach" {
+  name = "admin-attach"
+  groups = [aws_iam_group.admin.name]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
