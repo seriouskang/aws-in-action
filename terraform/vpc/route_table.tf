8653736ce993833ac2e8e07a0644f68cc7a2403b
seriouskang <scdean110@gmail.com>
2024-11-16 19:22:34 +0900

    feat: route table association

--- terraform/vpc/route_table.tf
+++ terraform/vpc/route_table.tf
@@ -10,3 +10,13 @@ resource "aws_route_table" "main-public" {
     Name = "main-public-1"
   }
 }
+
+resource "aws_route_table_association" "main-public-1-a" {
+  subnet_id = aws_subnet.main-public-1.id
+  route_table_id = aws_route_table.main-public.id
+}
+
+resource "aws_route_table_association" "main-public-2-a" {
+  subnet_id = aws_subnet.main-public-2.id
+  route_table_id = aws_route_table.main-public.id
+}
