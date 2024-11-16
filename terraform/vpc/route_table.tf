f0d35b61c0e25b5f93dd375a8f74637dfc27f14f
seriouskang <scdean110@gmail.com>
2024-11-16 19:49:40 +0900

    feat: private rtb 신규

--- terraform/vpc/route_table.tf
+++ terraform/vpc/route_table.tf
@@ -1,3 +1,4 @@
+# public
 resource "aws_route_table" "main-public" {
   vpc_id = aws_vpc.main.id
 
@@ -19,4 +20,18 @@ resource "aws_route_table_association" "main-public-1-a" {
 resource "aws_route_table_association" "main-public-2-a" {
   subnet_id = aws_subnet.main-public-2.id
   route_table_id = aws_route_table.main-public.id
+}
+
+
+# private
+resource "aws_route_table" "main-priavte" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "main-private-1"
+  }
 }
