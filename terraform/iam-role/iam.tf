6f02b688d58025f3d1fef60c3563f5e915a385f5
seriouskang <scdean110@gmail.com>
2024-11-17 09:07:43 +0900

    feat(iam-role): iam-role policy 신규

--- terraform/iam-role/iam.tf
+++ terraform/iam-role/iam.tf
@@ -17,3 +17,25 @@ resource "aws_iam_role" "s3-test-bucket-role" {
 EOF
 
 }
+
+resource "aws_iam_role_policy" "s3-test-bucket-role-policy" {
+  name = "s3-test-bucket-role-policy"
+  role = aws_iam_role.s3-test-bucket-role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "s3:*"
+            ],
+            "Resource": [
+              "arn:aws:s3:::test-bucket-241117",
+              "arn:aws:s3:::test-bucket-241117/*"
+            ]
+        }
+    ]
+}
+EOF
+}
